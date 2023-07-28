c5257797d9d552df86454f1beba93d55180db3d5
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-07-28 16:50:03 +0530

    first commit

---
+++ IRechargable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment18
+{
+    public interface IRechargeable
+    {
+        void Charge(int minutes);
+    }
+}
